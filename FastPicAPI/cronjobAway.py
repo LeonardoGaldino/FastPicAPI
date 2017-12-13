da0dc5a2907e9c867968108ccfda52b3eefd0744
LeonardoGaldino <lcgm@cin.ufpe.br>
2017-12-13 19:53:13 -0300

    feat (CronJob): CronJob that runs every minute to remove away players
    Implements cronjob to remove away players.

---
+++ FastPicAPI/cronjobAway.py
@@ -0,0 +1,27 @@
+from crontab import CronTab
+import os
+
+path = os.path.dirname(os.path.abspath(__file__))
+prev_path = os.path.dirname(path)
+file_name = 'manage.py'
+command_name = 'removeAwayUsers'
+full_path = os.path.join(path, file_name)
+log_file_name1 = 'logFileSourceAway'
+log_file_err1 = 'logFileErrSourceAway'
+log_file_name2 = 'logFilePythonAway'
+log_file_err2 = 'logfileErrPythonAway'
+out1 = '> ' + os.path.join(path, log_file_name1) + ' 2> ' + os.path.join(path, log_file_err1)
+out2 = '> ' + os.path.join(path, log_file_name2) + ' 2> ' + os.path.join(path, log_file_err2)
+activate_venv = 'source ' + prev_path + '/venv/bin/activate ' + out1
+command = 'python ' + full_path + ' ' + command_name + ' ' + out2
+
+# user parameter should be your PC's user!
+cron = CronTab(user='lcgm')
+cron.env['SHELL'] = '/bin/bash'
+cron.env['PATH'] = '/home/lcgm/workspace/Repositories/FastPic/venv/bin:/home/lcgm/bin:/home/lcgm/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
+
+job = cron.new(command=activate_venv + ' && '+command)
+
+job.minute.every(1)
+
+cron.write()
